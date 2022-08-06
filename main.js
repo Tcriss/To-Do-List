7ab4e8f9df101c608e2ffcfcfb5bb086d238a987
Tcriss <do.cristian.tr@hotmail.com>
2022-08-05 20:18:04 -0400

    Eliminar tareas almacenadas completado

--- main.js
+++ main.js
@@ -2,34 +2,32 @@ const input = document.querySelector('input');
 const addBtn = document.querySelector('.btnadd');
 const ul = document.querySelector('ul');
 const empty = document.querySelector('.empty');
-let tasks = [];
-let id = Date.now();
 
 addBtn.addEventListener("click", (e) => {
+    //Prevenir que la pagina recargue
     e.preventDefault();
+
+    //Recoge el texto del campo
     const text = input.value;
 
     if (text !== "") {
-
-        const task = {
-            text,
-            id
-        }
-        tasks = [...tasks,task]
-
-        localStorage.setItem('tasks',JSON.stringify(tasks));
+        //aqui se crea un nuevo list item
+        const li = document.createElement('li');
+        //se crea un parrafo para ser trasladado al list item anterior
         const p = document.createElement('p');
         p.textContent = text;
-              
-        const li = document.createElement('li');
+        
+        //aqui finalmente se inserta el Li
         li.appendChild(p);
         li.appendChild(DeleteBtn());
         ul.appendChild(li);
         
         input.value = "";
         empty.style.display = "none";
-        ul.style.height = "30vh";
+    } else {
+        alert("No puedes agregar un campo vacio");
     }
+    
 });
 
 function showTasks(){
@@ -53,15 +51,17 @@ function DeleteBtn() {
     const deleteBtn = document.createElement("button");
     
     deleteBtn.textContent = "x";
+    deleteBtn.setAttribute("id", id);
     deleteBtn.className = "btndelete";
     
     deleteBtn.addEventListener("click", (e) => {
-        //agarra el LI del boton eleminar
-        const item = e.target.parentElement;
-        ul.removeChild(item);
-
+        onst task = e.target.parentElement;
+        ul.removeChild(task);
+        let tasks = JSON.parse( localStorage.getItem('tasks'))
+        tasks.splice(e, 1);
+        console.log(tasks)
+        localStorage.setItem('tasks', JSON.stringify(tasks));
         const items = document.querySelectorAll("li");
-
         if (items.length === 0){
             empty.style.display = "block";
         }
