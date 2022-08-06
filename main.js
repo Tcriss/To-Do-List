ba82f1606cbc1ee5a067b4099b40031f1fc7d46e
Tcriss <do.cristian.tr@hotmail.com>
2022-08-05 20:49:32 -0400

    Implementadas las mejoras de las animaciones de la UX

--- main.js
+++ main.js
@@ -2,32 +2,36 @@ const input = document.querySelector('input');
 const addBtn = document.querySelector('.btnadd');
 const ul = document.querySelector('ul');
 const empty = document.querySelector('.empty');
+let tasks = [];
+let id = Date.now();
 
 addBtn.addEventListener("click", (e) => {
-    //Prevenir que la pagina recargue
     e.preventDefault();
-
-    //Recoge el texto del campo
     const text = input.value;
 
     if (text !== "") {
-        //aqui se crea un nuevo list item
-        const li = document.createElement('li');
-        //se crea un parrafo para ser trasladado al list item anterior
+
+        const task = {
+            text,
+            id
+        }
+        tasks = [...tasks,task]
+
+        localStorage.setItem('tasks',JSON.stringify(tasks));
         const p = document.createElement('p');
         p.textContent = text;
-        
-        //aqui finalmente se inserta el Li
+              
+        const li = document.createElement('li');
         li.appendChild(p);
         li.appendChild(DeleteBtn());
         ul.appendChild(li);
         
         input.value = "";
         empty.style.display = "none";
+        ul.style.height = "30vh";
     } else {
         Message('','Alto ahi!','No puedes agregar listas vacias');
     }
-    
 });
 
 function showTasks(){
@@ -47,13 +51,10 @@ function showTasks(){
 }
 
 function DeleteBtn() {
-    
     const deleteBtn = document.createElement("button");
-    
-    deleteBtn.textContent = "x";
+    deleteBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="30" height="25"><path d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z"/><path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z"/><path d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"/></svg>'
     deleteBtn.setAttribute("id", id);
     deleteBtn.className = "btndelete";
-    
     deleteBtn.addEventListener("click", (e) => {
         const task = e.target.parentElement;
         ul.removeChild(task);
@@ -62,8 +63,9 @@ function DeleteBtn() {
         console.log(tasks)
         localStorage.setItem('tasks', JSON.stringify(tasks));
         const items = document.querySelectorAll("li");
-        if (items.length === 0){
+        if (items.length == 0){
             empty.style.display = "block";
+            ul.style.height = "1vh";
         }
     });
     return deleteBtn;
